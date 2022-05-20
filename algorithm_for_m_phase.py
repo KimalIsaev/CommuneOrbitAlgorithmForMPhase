15f8cdab777197e8feb55bf3b41dc77a44f132c9
KimalIsaev <kimchikumnik@gmail.com>
2022-05-20 06:04:35 +0300

    fixed percision

--- algorithm_for_m_phase.py
+++ algorithm_for_m_phase.py
@@ -25,19 +25,26 @@ def get_rabp(m, n, sigma, lam, qs, mus, r0, r2, x_n, percision):
     B_ptr = python_square_matrix_to_cffi_array(B)
     K_ptr = python_square_matrix_to_cffi_array(K)
     I_ptr = python_square_matrix_to_cffi_array(I)
-    a_b = ffi.new("double[]", x_n)
-    a = ffi.new("double[]", x_n)
-    b = ffi.new("double[]", x_n)
-    r = ffi.new("double[]", x_n*matrix_dim)
+    true_n = x_n*percision
+    true_eps = sigma/percision
+    a_b = ffi.new("double[]", true_n)
+    a = ffi.new("double[]", true_n)
+    b = ffi.new("double[]", true_n)
+    r = ffi.new("double[]", true_n*matrix_dim)
     lib.charge_a_b_integral(
         a_b, a, b, r,
         A_ptr, B_ptr, K_ptr, I_ptr,
-        matrix_dim, x_n, percision)
-    np_a_b = np.array(list(a_b))
-    np_a = np.array(list(a))
+        matrix_dim, true_n, true_eps)
+    np_a_b = np.array([d 
+        for i,d in enumerate(list(a_b))
+        if ((i % percision) == 0)])
+    np_b_ = np.array([d 
+        for i,d in enumerate(list(b))
+        if ((i % percision) == 0)])
     np_b = np.array(list(b))
+    np_a = np.array(list(a))
     np_r = np.array(list(r))
-    np_pi = np.exp(2*np_a_b/sigma)/np_b
+    np_pi = np.exp(2*np_a_b/sigma)/np_b_
 #    print(np_pi)
     np_orbit_distribution = np_pi/sum(np_pi)
     r_out = np_r.astype(np.float64)
@@ -80,7 +87,7 @@ def main():
             print("negative argument")
             return 1
     x_n = int(sys.argv[5])
-    percision = float(sys.argv[6])
+    percision = int(sys.argv[6])
     n = int(sys.argv[7])
     lambda_value = float(sys.argv[8])
     sigma_value = float(sys.argv[9])
