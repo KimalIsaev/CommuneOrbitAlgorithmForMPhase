53edef849a1977438cb42b61cf291521a450de42
KimalIsaev <kimchikumnik@gmail.com>
2022-05-22 23:10:15 +0300

    added percision

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+ceildiv(){echo "$1 $2" | awk '{print int( ($1/$2) + 1 )}'}
+
 parent_dir=$(dirname $0) 
 r_dir=$parent_dir/r
 a_dir=$parent_dir/a
@@ -6,9 +8,14 @@ p_dir=$parent_dir/p
 
 mkdir -p $r_dir $a_dir $b_dir $p_dir 
 
+algorithm=$1
 draw_script=$2
 x_n=$3
-percision=$4
+sigma=$4
+param_file=$5
+
+percision=0.0005
+subdivisions=$(ceildiv $sigma $percision)
 
 time=$(date +"%d:%m:%Y_%H:%M:%S")
 i=0
@@ -17,13 +24,14 @@ do
     if [ -z "${line}" ]; then
         exit 0
     fi;
-	name=$(basename $5 .csv)'_'$i'_'$time
-	python3 $1 $r_dir/$name.txt $a_dir/$name.txt \
-        $b_dir/$name.txt $p_dir/$name.txt $x_n $percision $line
+	name=$(basename $param_file .csv)'_'$i'_'$time
+	python3 $algorithm $r_dir/$name.txt $a_dir/$name.txt \
+        $b_dir/$name.txt $p_dir/$name.txt $x_n $subdivisions $sigma $line
 	python3 $draw_script $r_dir/$name.txt $r_dir/$name.png $percision
 	python3 $draw_script $a_dir/$name.txt $a_dir/$name.png $percision
 	python3 $draw_script $b_dir/$name.txt $b_dir/$name.png $percision
 	python3 $draw_script $p_dir/$name.txt $p_dir/$name.png
 	export i=$(($i+1))
 	echo $name
-done < $5
+done < $param_file
+
